573d748fe858963594236267d3fe21a52ccf1cfd
Brian <brianmartinezdothe@gmail.com>
2026-01-29 16:05:18 +0000

    Week 3

--- Week 2/test.c
+++ Week 2/test.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
-
+#include<time.h>
+#include<stdlib.h>
 
 int main()
 {
-    int var1;
+    
 
-    printf("Enter a number :");
-    scanf("%d",&var1);
-    printf("%d",var1); 
+    for (int i = 1; i <7; i++)
+    {
+        int random_num = rand() % 7; // Generate a random number between 0 and 6
+        
 
-    return 0;
-}
+
+        return 0;
+    }
