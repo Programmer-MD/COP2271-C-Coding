560707e866aa4f3c5b4be32ff77b0ae6e559253e
Brian <brianmartinezdothe@gmail.com>
2026-05-04 18:22:23 +0000

    Problem 3 done

--- Week Finals/Coding Exercise.c
+++ Week Finals/Coding Exercise.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
-Student Grade Calculator
+1. Student Grade Calculator
+
 int main()
 {
     int total_score = 0;
@@ -37,4 +38,100 @@ int main()
         
     return 0;
 }
+
+2. Coding Exercises
+
+void swap_by_value(int a, int b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    printf("After swap by value: a = %d, b = %d\n(FUNCTION view, can only swap with pointers)\n", a, b);
+    
+}
+void swap_by_pointer(int *a, int *b)
+{
+    int temp;
+    temp =  *a;
+   *a = *b;
+   *b = temp;
+
+
+}
+
+int main()
+{
+    int a = 1, b = 2;
+    int ch = 0;
+
+    printf("What way do you want to swap?\n1. By value\n2. By Pointer\n");
+    scanf("%d", &ch);
+    if (ch ==1)
+    {
+        printf("Before swap by value: a = %d, b = %d\n", a, b);
+        swap_by_value(a, b);
+        printf("After swap by value: a = %d, b = %d\n(In main variables are unchanged only its copies are", a, b);
+    }
+    else  if (ch ==2)
+    {
+        printf("Before swap by pointer: a = %d, b = %d\n", a, b);
+        swap_by_pointer(&a, &b);
+        printf("After swap by pointer: a = %d, b = %d\n", a, b);
+    }
+    else
+    printf("Error");
+
+    return 0;
+}
+
+3. Number Classification
+
+int main()
+{
+    int num;
+    printf("Please enter a number:  ");
+    scanf("%d", &num);
+    if (num < 0)
+        printf("\nThe number %d is Negative", num );
+    else if (num == 0)
+        printf("\nThe number is 0");
+    else if (num > 0)
+        printf("\nThe number %d is Positive", num);
+    else
+        printf("Please enter a integer value");
+
+    int even_check =  num % 2;
+    if (even_check ==  0)
+        printf("\nand...Even\n", num);
+    else
+        printf("\nand...Odd\n", num);
+
+
+    return 0;
+}
+
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
