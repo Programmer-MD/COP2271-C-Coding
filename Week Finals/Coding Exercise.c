3d4661573425d6c75886f195d07ffb07a6b48410
Brian <brianmartinezdothe@gmail.com>
2026-05-04 17:24:03 +0000

    Problem 1 Done

--- Week Finals/Coding Exercise.c
+++ Week Finals/Coding Exercise.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
+Student Grade Calculator
 int main()
 {
     int total_score = 0;
     int score = 0;
     int ctr = 0;
-    for(int i; i  >  4; i++)
+    for(int i = 0; i < 3; i++)
     {
         printf("Please enter your exam grade (up to 3)\n");
         scanf("%d", &score);
+        if (score < 0 || score > 100) 
+        {
+            i--;
+            printf("Invalid score. Please enter a score between 0 and 100.\n");
+            continue;
+        }
         total_score += score;
         ctr++;
     }
+
     int ave = total_score/ctr;
     if (ave <= 100 && ave >= 90)
         printf("Your average is: %d\nGrade: A\n", ave);
@@ -24,7 +32,9 @@ int main()
         printf("Your average is: %d\nGrade: D\n",ave);
     else if (ave < 59)
         printf("Your average is: %d\nGrade: F\n",ave);
+    else
+        printf("Invalid average score.\n");
         
-
     return 0;
 }
+*/
